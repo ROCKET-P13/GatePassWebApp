99dd236091603acbce58deb3d95c3cf270a23602
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-01-13 07:50:45 -0500

    update: cleanup

--- src/Components/Dashboard/DashboardDrawer.jsx
+++ src/Components/Dashboard/DashboardDrawer.jsx
@@ -5,12 +5,9 @@ import {
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
-	Divider,
 	Drawer
 } from '@mui/material';
 
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import SettingsIcon from '@mui/icons-material/Settings';
 import PersonIcon from '@mui/icons-material/Person';
 import _ from 'lodash';
@@ -42,19 +39,6 @@ export const DashboardDrawer = () =>  {
 					role='presentation'
 					onClick={toggleDrawer}
 				>
-					<List>
-						{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-							<ListItem key={text} disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-									</ListItemIcon>
-									<ListItemText primary={text} />
-								</ListItemButton>
-							</ListItem>
-						))}
-					</List>
-					<Divider />
 					<List>
 						{
 							_.chain(DrawerItems).keys().map((drawerItemKey) => (
