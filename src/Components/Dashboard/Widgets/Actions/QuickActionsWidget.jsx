92727b59ec71883ff5cf0777198cadbcee2039a7
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-01-21 08:28:57 -0500

    update: rm some stuff

--- src/Components/Dashboard/Widgets/Actions/QuickActionsWidget.jsx
+++ src/Components/Dashboard/Widgets/Actions/QuickActionsWidget.jsx
@@ -6,9 +6,6 @@ export const QuickActionsWidget = () => {
 		<WidgetCard title="Quick Actions">
 			<Stack spacing={1}>
 				<Button variant="contained">Create Event</Button>
-				<Button variant="outlined">Open Check-in</Button>
-				<Button variant="outlined">View Waivers</Button>
-				<Button variant="text">Invite Staff</Button>
 			</Stack>
 		</WidgetCard>
 	);
