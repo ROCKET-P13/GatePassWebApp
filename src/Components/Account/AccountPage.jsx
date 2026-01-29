ef40e41fc67a4951b1bda70b2fbd90d13dd9f443
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-01-29 08:55:13 -0500

    update: rm unnecessary import from AccountsPage

--- src/Components/Account/AccountPage.jsx
+++ src/Components/Account/AccountPage.jsx
@@ -1,6 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Avatar, Box, Button, Paper, Stack, Typography } from '@mui/material';
-import { Routes } from '../../Common/routes';
 
 export const AccountPage = () => {
 	const { user, isLoading, logout } = useAuth0();
