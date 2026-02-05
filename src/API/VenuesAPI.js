4e2efda31b9ee7a6fbb7b11db4de893c8aa0feda
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-02-05 07:32:21 -0500

    feat: VenuesAPI

---
+++ src/API/VenuesAPI.js
@@ -0,0 +1,26 @@
+import { APIClient } from './APIClient';
+
+export class VenuesAPI {
+	#url = '/venues';
+	#apiClient;
+	constructor (params = {}) {
+		this.#apiClient = params.apiClient ?? new APIClient({
+			getAccessToken: params.getAccessToken,
+		});
+	}
+
+	async create ({ name, email, addressLine1, addressLine2, phoneNumber, city, state }) {
+		return await this.#apiClient.post({
+			url: this.#url,
+			body: {
+				name,
+				email,
+				addressLine1,
+				addressLine2,
+				phoneNumber,
+				city,
+				state,
+			},
+		});
+	}
+}
