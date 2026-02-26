d799933d0fc19cb8849f319d8d4809b8c25583c0
Leo <135278016+ROCKET-P13@users.noreply.github.com>
2026-02-26 12:45:12 -0500

    update: rm Material UI theming

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { ThemeProvider } from '@mui/material/styles';
 import { Auth0Provider } from '@auth0/auth0-react';
 import '@fontsource/inter';
 import './index.css';
-import theme from './theme';
 import { AppRouter } from './AppRouter';
 
 createRoot(document.getElementById('root')).render(
@@ -22,11 +20,9 @@ createRoot(document.getElementById('root')).render(
 			cacheLocation="localstorage"
 			useRefreshTokens
 		>
-			<ThemeProvider theme={theme}>
-				<LocalizationProvider dateAdapter={AdapterDayjs}>
-					<AppRouter />
-				</LocalizationProvider>
-			</ThemeProvider>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<AppRouter />
+			</LocalizationProvider>
 		</Auth0Provider>
 	</StrictMode>
 );
